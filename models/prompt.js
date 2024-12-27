fc56a3a02a39741286397397cc9017a22238ad4d
Marcelo Oroquieta <oroquieta.m@gmail.com>
2024-12-27 18:43:09 -0300

    feat(model): creation of prompt model

---
+++ models/prompt.js
@@ -0,0 +1,23 @@
+import { Schema, model, models } from "mongoose";
+
+const PromptSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is required"],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is required"],
+  },
+});
+
+/* Mongose stores the models created in models, so before creating the model
+with model(), we must first check if the model does not already exist, to
+avoid generating it every time the API is called. */
+const Prompt = models.Prompt || model("Prompt", PromptSchema);
+
+export default Prompt;
