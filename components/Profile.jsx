4fe444932051573813050226d0fb86ed16a71d76
Marcelo Oroquieta <oroquieta.m@gmail.com>
2024-12-27 19:02:35 -0300

    feat(profile): add profile functionality

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,5 +1,19 @@
-const Profile = () => {
-  return <div>Profile</div>;
+import PromptCardList from "./PromptCardList";
+
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+  return (
+    <section className="w-full flex justify-center items-center flex-col">
+      <h1 className="head_text text-left self-baseline">
+        <span className="blue_gradient">{name} Profile</span>
+      </h1>
+      <p className="desc text-left self-baseline">{desc}</p>
+      <PromptCardList
+        data={data}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+      />
+    </section>
+  );
 };
 
 export default Profile;
