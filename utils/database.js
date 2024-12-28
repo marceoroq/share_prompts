2e648c0e6b41e1338c0ccc83b5f47a176ff44dd0
Marcelo Oroquieta <oroquieta.m@gmail.com>
2024-12-27 21:26:10 -0300

    chore: add validation of env reading for vercel deploy

--- utils/database.js
+++ utils/database.js
@@ -11,14 +11,19 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "share_prompts",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const MONGODB_URI = process.env.MONGODB_URI;
+    if (!MONGODB_URI) {
+      console.error("MONGODB_URI was not found");
+    } else {
+      await mongoose.connect(process.env.MONGODB_URI, {
+        dbName: "share_prompts",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
 
-    isConnected = true;
-    console.log("[i] MongoDB Connected");
+      isConnected = true;
+      console.log("[i] MongoDB Connected");
+    }
   } catch (error) {
     console.error(error);
   }
